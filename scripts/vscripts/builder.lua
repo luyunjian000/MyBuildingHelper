ede7717179c96a40b84f05b23b1744a33fad39ba
luyunjian000 <1825208171@qq.com>
2022-08-15 17:10:25 +0800

    兵营设置出兵点
    兵营设置出兵点

--- scripts/vscripts/builder.lua
+++ scripts/vscripts/builder.lua
@@ -176,3 +176,7 @@ function SendErrorMessage( pID, string )
     Notifications:Bottom(pID, {text=string, style={color='#E62020'}, duration=2})
     EmitSoundOnClient("General.Cancel", PlayerResource:GetPlayer(pID))
 end
+
+function SelectBornPosition(event)
+    print(event);
+end
